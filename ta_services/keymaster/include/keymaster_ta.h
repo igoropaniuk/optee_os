f2fbcb21c1e5abf436b7d5a70fa74a0f3636e4f6
Igor Opaniuk <igor.opaniuk@linaro.org>
2018-06-06 18:39:16 +0300

    keymaster: add CMD definitions
    Signed-off-by: Igor Opaniuk <igor.opaniuk@linaro.org>

--- ta_services/keymaster/include/keymaster_ta.h
+++ ta_services/keymaster/include/keymaster_ta.h
@@ -15,6 +15,108 @@
  *
  * in	params[0].memref: entropy input data
  */
-#define KEYMASTER_CMD_ADD_RNG_ENTROPY	0
+#define KEYMASTER_CMD_ADD_RNG_ENTROPY			0
+
+/*
+ * Generates a new cryptographic key, specifying associated authorizations,
+ * which are permanently bound to the key.
+ *
+ * params TODO:
+ */
+#define KEYMASTER_CMD_GENERATE_KEY			1
+
+/*
+ * Imports key material into Keymaster hardware.
+ *
+ * params TODO:
+ */
+#define KEYMASTER_CMD_IMPORT_KEY 			2
+
+/*
+ * Exports a public key from a Keymaster RSA or EC key pair.
+ *
+ * params TODO:
+ */
+#define KEYMASTER_CMD_EXPORT_KEY			3
+
+/*
+ * Generates a new cryptographic key, specifying associated authorizations,
+ * which are permanently bound to the key.
+ *
+ * params TODO:
+ */
+#define KEYMASTER_CMD_ATTEST_KEY			4
+/*
+ * Generates a new cryptographic key, specifying associated authorizations,
+ * which are permanently bound to the key.
+ *
+ * params TODO:
+ */
+#define KEYMASTER_CMD_UPGRADE_KEY			5
+/*
+ * Generates a new cryptographic key, specifying associated authorizations,
+ * which are permanently bound to the key.
+ *
+ * params TODO:
+ */
+#define KEYMASTER_CMD_DELETE_KEY			6
+/*
+ * Generates a new cryptographic key, specifying associated authorizations,
+ * which are permanently bound to the key.
+ *
+ * params TODO:
+ */
+#define KEYMASTER_CMD_DELETE_ALL_KEYS			7
+/*
+ * Generates a new cryptographic key, specifying associated authorizations,
+ * which are permanently bound to the key.
+ *
+ * params TODO:
+ */
+#define KEYMASTER_CMD_GET_KEY_CHARACTERISTICS		8
+/*
+ * Generates a new cryptographic key, specifying associated authorizations,
+ * which are permanently bound to the key.
+ *
+ * params TODO:
+ */
+#define KEYMASTER_CMD_DESTROY_ATTESTATION_IDS		9
+
+/*
+ * Begins a cryptographic operation, using the specified key, for the specified
+ * purpose, with the specified parameters (as appropriate), and returns an
+ * operation handle that is used with update and finish to complete the
+ * operation. The operation handle is also used as the "challenge" token in
+ * authenticated operations, and for such operations is included in the
+ * challenge field of the authentication token.
+ *
+ * params TODO:
+ */
+
+#define KEYMASTER_CMD_BEGIN				10
+
+/*
+ * Provides data to process in an ongoing operation started with begin.
+ * The operation is specified by the operationHandle parameter.
+ *
+ * params TODO:
+ */
+#define KEYMASTER_CMD_UPDATE				11
+
+/*
+ * Finishes an ongoing operation started with begin, processing all of
+ * the as-yet-unprocessed data provided by update(s).
+ * This method is the last one called in an operation, so all processed
+ * data is returned.
+ *
+ * params TODO:
+ */
+#define KEYMASTER_CMD_FINISH				12
+/*
+ * Aborts the in-progress operation
+ *
+ * params TODO:
+ */
+#define KEYMASTER_CMD_ABORT				13
 
 #endif /*__KEYMASTER_TA_H*/
